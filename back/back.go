fbc7a8d35f2401f77e6cb799a2401c9de88d3a05
hsmtkk <hsmtkk@gmail.com>
2023-05-13 09:52:59 +0000

    up

--- back/back.go
+++ back/back.go
@@ -1,3 +1,15 @@
-package main
+package function
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("EntryPoint", EntryPoint)
+}
+
+func EntryPoint(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
--- main.ts
+++ main.ts
@@ -1,22 +1,120 @@
 import { Construct } from "constructs";
-import { App, TerraformStack, GcsBackend } from "cdktf";
-import * as google from '@cdktf/provider-google';
+import { App, TerraformStack, GcsBackend, TerraformAsset, AssetType } from "cdktf";
+import * as google from "@cdktf/provider-google";
+import * as path from "path";
 
-const project = "glowing-engine";
+const project = "glowing-engine-386523";
 const region = "us-central1";
-const cdktfBackend = `cdktf-bucket-${project}`;
+const repository = "glowing-engine";
+const cdktfBucket = "cdktf-bucket-glowing-engine";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     new GcsBackend(this, {
-        bucket: cdktfBackend,
+        bucket: cdktfBucket,
     });
 
     new google.provider.GoogleProvider(this, "google", {
         project,
         region,
+    });
+
+    new google.cloudbuildTrigger.CloudbuildTrigger(this, "trigger", {
+        filename: "cloudbuild.yaml",
+        github: {
+            owner: "hsmtkk",
+            name: repository,
+            push: {
+                branch: "main",
+            },
+        },
+    });
+
+    new google.artifactRegistryRepository.ArtifactRegistryRepository(this, "registry", {
+        format: "DOCKER",
+        location: region,
+        repositoryId: "registry",
+    });
+
+    const gourmetSearchAPIKey = new google.secretManagerSecret.SecretManagerSecret(this, 'gourmetSearchAPIKey', {
+        secretId: "gourmet-search-api-key",
+        replication: {
+            automatic: true,
+        },
+    });
+
+    const frontRunner = new google.serviceAccount.ServiceAccount(this, 'frontRunner', {
+        accountId: "front-runner",
+    });
+
+    const backRunner = new google.serviceAccount.ServiceAccount(this, 'backRunner', {
+        accountId: "back-runner",
+    });
+
+    const backService = new google.cloudRunV2Service.CloudRunV2Service(this, 'backService', {
+        location: region,
+        name: 'back-service',
+        template: {
+            containers: [{
+                env: [{
+                    name: "GOURMET_SEARCH_API_KEY",
+                    valueSource: {
+                        secretKeyRef: {
+                            secret: gourmetSearchAPIKey.secretId,
+                            version: "latest",
+                        },
+                    },
+                }],
+                image: "us-docker.pkg.dev/cloudrun/container/hello",
+            }],
+            serviceAccount: backRunner.email,
+        },
+    });
+
+    const asset = new TerraformAsset(this, "asset", {
+        path: path.resolve("back"),
+        type: AssetType.ARCHIVE,
+    });
+
+    const assetBucket = new google.storageBucket.StorageBucket(this, "assetBucket", {
+        lifecycleRule: [{
+          condition: {
+            age: 1,
+          },
+          action: {
+            type: "Delete",
+          },
+        }],
+        location: region,
+        name: `asset-bucket-${project}`,
+    });
+
+    const assetObject = new google.storageBucketObject.StorageBucketObject(this, "assetObject", {
+        bucket: assetBucket.name,
+        name: asset.assetHash,
+        source: asset.path,
+    });
+
+    new google.cloudfunctions2Function.Cloudfunctions2Function(this, "frontService", {
+        buildConfig: {
+            runtime: "go120",
+            entryPoint: "EntryPoint",
+            source: {
+                storageSource: {
+                    bucket: assetBucket.name,
+                    object: assetObject.name,
+                },
+            },
+        },
+        name: "front-service",
+        serviceConfig: {
+            environmentVariables: {
+                BACK_SERVICE_URL: backService.uri,
+            },
+            serviceAccountEmail: frontRunner.email,
+        },
     });
 
   }
