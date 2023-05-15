3a1e546d8d81699241b6115dedc8cffa0e44d392
hsmtkk <hsmtkk@gmail.com>
2023-05-15 00:43:38 +0000

    add auth

--- front/front.py
+++ front/front.py
@@ -1,18 +1,31 @@
 import os
 
+import google.auth.transport.requests
+import google.oauth2.id_token
 import pandas as pd
 import requests
 import streamlit as st
 
-with st.form("form"):
+def main() -> None:
     back_service_url = os.environ["BACK_SERVICE_URL"]
-    keyword = st.text_input(label="キーワード", value="新宿")
-    submitted = st.form_submit_button("送信")
+    id_token = get_id_token(back_service_url)
+    headers = {"Authorization", f"Bearer {id_token}"}
 
-    if submitted:
-        params = {"keyword": keyword}
-        resp = requests.get(back_service_url, params=params)
-        if resp.status_code < 200 or resp.status_code >= 300:
-            st.error(f"HTTP status code {resp.status_code}: {resp.text}")
-        resp_dict = resp.json()
-        st.write(resp_dict)
+    with st.form("form"):
+        keyword = st.text_input(label="キーワード", value="新宿")
+        submitted = st.form_submit_button("送信")
+
+        if submitted:
+            params = {"keyword": keyword}
+            resp = requests.get(back_service_url, headers=headers, params=params)
+            if resp.status_code < 200 or resp.status_code >= 300:
+                st.error(f"HTTP status code {resp.status_code}: {resp.text}")
+            resp_dict = resp.json()
+            st.write(resp_dict)
+
+def get_id_token(audience:str) -> str:
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
+    return id_token
+
+main()
